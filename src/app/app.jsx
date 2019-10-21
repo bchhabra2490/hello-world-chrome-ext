c3eb6edbb444fe7f000ef63dd3cd8229317f2471
Bharat Chhabra <bchhabra2490@gmail.com>
2019-10-21 07:57:46 +0530

    Chrome extension

---
+++ src/app/app.jsx
@@ -0,0 +1,10 @@
+import { h, Component } from 'preact';
+import './app.scss';
+
+class App extends Component{
+	render(){
+		return <div id="app-root"><h1>Preact Chrome Extension Starter</h1></div>
+	}
+}
+
+export default App;
